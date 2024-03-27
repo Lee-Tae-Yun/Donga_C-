00cfc26fc06ed2e8b3c2013f9d81fd4f63c7808b
이태윤 <58839422+Lee-Tae-Yun@users.noreply.github.com>
2024-03-27 18:16:36 +0900

    Add files via upload
    ex.01

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "학번 : ";
+    int number ;
+    cin >> number;
+    cout << "이름 : ";
+    char name[11];
+    cin >> name;
+    cout << "학번 : " << number << " 이름 : " << name << "\n";
+
+    return 0;
+}
